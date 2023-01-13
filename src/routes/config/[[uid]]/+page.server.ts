6478e64af1dd6ae5a028f453ff2f2070786d7f39
apop880 <ajpopoutsis@gmail.com>
2023-01-13 22:27:02 +0000

    Initial support for creating configurations

--- src/routes/config/[[uid]]/+page.server.ts
+++ src/routes/config/[[uid]]/+page.server.ts
@@ -8,8 +8,30 @@ export const load = (async ({ params }) => {
     return { configuration: body }
 }) satisfies PageServerLoad
 
-// export const actions: Actions = {
-//     default: async (event) => {
-
-//     }
-// }
+export const actions: Actions = {
+    default: async ({ request, params }) => {
+        
+        const data = await request.formData();
+        const name = data.get('name') as string;
+        const weatherEntity = data.get('weatherEntity');
+        const backgroundMusicEntity = data.get('backgroundMusicEntity');
+        const backgroundMusicFile = data.get('backgroundMusicFile');
+        const result = await prisma.configuration.upsert({
+            create: {
+                name: name,
+                weatherEntity: weatherEntity as string ?? null,
+                backgroundMusicEntity: backgroundMusicEntity as string ?? null,
+                backgroundMusicFile: backgroundMusicFile as string ?? null
+            },
+            where: {
+                uid: params.uid ?? "new"
+            },
+            update: {
+                name: name,
+                weatherEntity: weatherEntity as string ?? null,
+                backgroundMusicEntity: backgroundMusicEntity as string ?? null,
+                backgroundMusicFile: backgroundMusicFile as string ?? null
+            }
+        })
+    }
+}
--- src/routes/config/[[uid]]/+page.svelte
+++ src/routes/config/[[uid]]/+page.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
     import type { PageData } from './$types';
     import { stateStore } from '$lib/apistore'
-    import { AppShell } from '@svelteuidev/core'
+    import { AppShell, Button } from '@svelteuidev/core'
     export let data: PageData;
 </script>
 
 <AppShell><slot>
 {#if $stateStore !== null}
-<form>
-    <label for="weather-select">Weather Entity</label>
-    <select name="weather" id="weather-select">
+<form method="POST">
+    <label for="name">Configuration Name</label>
+    <input type="text" id="name" name="name" required>
+    <label for="weather-entity">Weather Entity</label>
+    <select name="weatherEntity" id="weather-entity">
         <option value="" selected={data.configuration?.weatherEntity === null}> </option>
         {#each Object.keys($stateStore) as entity}
             {#if entity.startsWith('weather')}
                 <option value={entity} selected={data.configuration?.weatherEntity === entity}>{entity}</option>
             {/if}
         {/each}
-    </select>
+    </select><br>
+    <label for="music-entity">Background Music Entity</label>
+    <select name="backgroundMusicEntity" id="music-entity">
+        <option value="" selected={data.configuration?.backgroundMusicEntity === null}> </option>
+        {#each Object.keys($stateStore) as entity}
+            {#if entity.startsWith('input_boolean')}
+                <option value={entity} selected={data.configuration?.backgroundMusicEntity === entity}>{entity}</option>
+            {/if}
+        {/each}
+    </select><br>
+    <label for="music-file">Background Music File</label>
+    <input type="text" id="music-file" name="backgroundMusicFile">
+    <Button>Save</Button>
 </form>
 {/if}
 </slot></AppShell>

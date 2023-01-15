b66e2d2eb043d06eec712203f4cf355fb080884a
apop880 <ajpopoutsis@gmail.com>
2023-01-15 06:54:59 +0000

    fix DATABASE_URL env var

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -3,6 +3,6 @@ host=$(bashio::services "mysql" "host")
 password=$(bashio::services "mysql" "password")
 port=$(bashio::services "mysql" "port")
 username=$(bashio::services "mysql" "username")
-DATABASE_URL="mysql://${username}:${password}@${host}:${port}/photodash?schema=public"
+export DATABASE_URL="mysql://${username}:${password}@${host}:${port}/photodash?schema=public"
 npx prisma db push
 node /build/index.js

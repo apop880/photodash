6fcc6131469727888d1cfc762fd5b3311ffc3084
apop880 <ajpopoutsis@gmail.com>
2023-01-15 06:51:11 +0000

    Prisma should be able to create the db if it doesn't exist

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -3,9 +3,6 @@ host=$(bashio::services "mysql" "host")
 password=$(bashio::services "mysql" "password")
 port=$(bashio::services "mysql" "port")
 username=$(bashio::services "mysql" "username")
-# Create database if not exists
-echo "CREATE DATABASE IF NOT EXISTS photodash;" \
-    | mysql -h "${host}" -P "${port}" -u "${username}" -p"${password}"
 DATABASE_URL="mysql://${username}:${password}@${host}:${port}/photodash?schema=public"
 npx prisma db push
 node /build/index.js

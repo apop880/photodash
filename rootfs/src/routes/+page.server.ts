2b23918f64f667a14ec2bc6ea18ffac8f1ea6d76
Alex Popoutsis <ajpopoutsis@gmail.com>
2023-01-15 23:54:28 -0600

    WIP improve Home Assistant onboarding process

--- rootfs/src/routes/+page.server.ts
+++ rootfs/src/routes/+page.server.ts
@@ -2,8 +2,8 @@ import prisma from "$lib/prisma";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async () => {
-    //let body = await prisma.configuration.findMany();
-    let body = await prisma.configuration.findMany({select: { uid: true, name: true }})
+    let hassBaseUrl = await prisma.baseSettings.findOne({})
+    let configurations = await prisma.configuration.findMany({select: { uid: true, name: true }})
     
-    return { configurations: body }
+    return { configurations: configurations }
 }) satisfies PageServerLoad

c2e7b374e7aaabdd56f2ba06cb128e0dfa7e7f03
apop880 <ajpopoutsis@gmail.com>
2023-01-18 04:29:21 +0000

    Remove baseSettings API route, no longer used

--- rootfs/src/routes/api/baseSettings/+server.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '$lib/prisma'
-import { json } from '@sveltejs/kit'
-
-export async function GET() {
-    let hassBaseUrl = await prisma.baseSettings.findFirst({where: {param: 'hassBaseUrl'}})
-    let token = await prisma.baseSettings.findFirst({where: {param: 'token', value: {not: ''}}})
-    
-    return json({hassBaseUrl: hassBaseUrl?.value, token: token?.value ?? null})
-  }

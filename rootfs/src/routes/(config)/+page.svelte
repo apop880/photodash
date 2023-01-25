49cd07a9b6ae69dcbb4dde081b630089f84b6ac1
apop880 <ajpopoutsis@gmail.com>
2023-01-25 05:41:28 +0000

    Improve main config page UI

--- rootfs/src/routes/(config)/+page.svelte
+++ rootfs/src/routes/(config)/+page.svelte
@@ -3,28 +3,36 @@
     import type { PageData } from './$types'
     import { getHassAuth } from '$lib/apistore';
     export let data: PageData;
-    let auth;
+    let loaded = false;
     if (!(data.baseSettings?.token) && browser) {
-        getHassAuth(data.baseSettings!.hassBaseUrl);
+        getHassAuth(data.baseSettings!.hassBaseUrl).then(res => {
+          if (res.data) {
+            loaded = true;
+          }
+        });
+    }
+    else if (browser) {
+      loaded = true;
     }
 </script>
 
-{#if data.configurations.length > 0}
-    <h2>Existing Configurations</h2>
-    <br>
-        {#each data.configurations as config}
-            <div class="grid grid-cols-3 gap-2 items-center">
-                <div>{config.name}</div>
-                <a href={`/${config.uid}`} class="btn btn-filled-primary">
-                  Load
-                </a>
-                <a href={`/config/${config.uid}`} class="btn btn-filled-primary">
-                  Edit
-                </a>
-              </div>
-        {/each}
-{:else}
-    <h2>No Existing Configurations</h2>
-{/if}
-<br><br>
-<a class="btn btn-filled-primary" href="/config">Create New Configuration</a>
+<div class="grid grid-cols-6 gap-x-2 gap-y-4 items-center max-w-fit mx-auto">
+  {#if loaded}
+  <div class="text-lg font-bold col-span-5">Existing Configurations</div>
+  <div><a class="btn btn-filled-primary" href="/config">Create New</a></div>
+  <hr class="col-span-6 h-1 bg-gray-400 border-0">
+  {#each data.configurations as config}
+    <div class="col-span-3 font-semibold">{config.name}</div>
+    <a href={`/${config.uid}`} class="btn btn-filled-primary">
+      Load
+    </a>
+    <a href={`/config/${config.uid}`} class="btn btn-filled-primary">
+      Edit
+    </a>
+    <div class="btn btn-disabled">Clone</div>
+    <hr class="col-span-6 h-1 bg-gray-400 border-0">
+  {/each}
+  {:else}
+  <div class="text-lg font-bold col-span-6 text-center">Connecting to Home Assistant...</div>
+  {/if}
+</div>

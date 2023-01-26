f4ce2f314953c13d30948a5e7eb3730de583020b
apop880 <ajpopoutsis@gmail.com>
2023-01-26 17:56:27 +0000

    Add swipe down to reveal app toolbar

--- rootfs/src/routes/(app)/[uid]/+page.svelte
+++ rootfs/src/routes/(app)/[uid]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { PageData } from './$types';
     import { stateStore } from '$lib/apistore'
+    import { swipe } from 'svelte-gestures';
     import MusicPlayer from './bgmusic.svelte';
 	import Slideshow from './Slideshow.svelte';
 	import Topbar from './topbar.svelte';
@@ -8,16 +9,25 @@
     let curview = null;
     let showSlideShow = true;
     let showMenu = false;
+    let showToolbar = false;
     let timer: string | number | NodeJS.Timeout | undefined;
 
-    const mainClick = () => {
-        console.log("mainClick")
+    const handleClick = () => {
         clearTimeout(timer);
         showMenu == true;
         timer = setTimeout(() => {
             showMenu = false;
             curview = null;
         })
+    }
+
+    const handleSwipe = (e: CustomEvent) => {
+        if (e.detail.direction === "bottom") {
+            showToolbar = true;
+        }
+        else {
+            showToolbar = false;
+        }
     }
 
     $: {
@@ -33,8 +43,20 @@
     }
 </script>
 
+{#if showToolbar}
+<div class="navbar bg-primary shadow-lg shadow-primary-content z-50 absolute">
+	<div class="flex-1">
+	  <a href="/" class="btn btn-ghost normal-case text-xl text-primary-content">Photodash</a>
+	</div>
+	<div class="flex-none">
+	  <a href="/settings" class="btn btn-square btn-ghost text-primary-content">
+		<iconify-icon icon="ion:settings-outline" height="36"></iconify-icon>
+	  </a>
+	</div>
+  </div>
+  {/if}
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<main class="h-screen w-screen overflow-y-hidden bg-transparent" on:click={mainClick}>
+<main class="h-screen w-screen overflow-y-hidden bg-transparent" on:click={handleClick} use:swipe={{ timeframe: 300, minSwipeDistance: 50, touchAction: 'none' }} on:swipe={handleSwipe}>
     <Topbar />
     {#if $stateStore !== null}
         {#if data.configuration?.backgroundMusicEntity && data.configuration?.backgroundMusicFile}

8843c3ba9d5b94d6b6750443c2fabecc9537a961
apop880 <ajpopoutsis@gmail.com>
2023-01-29 03:34:18 +0000

    Swipeable navbar, and initial scaffolding for managing editing and loading cards

--- rootfs/src/routes/(app)/[uid]/+page.svelte
+++ rootfs/src/routes/(app)/[uid]/+page.svelte
@@ -2,14 +2,23 @@
     import type { PageData } from './$types';
     import { stateStore } from '$lib/apistore'
     import { swipe } from 'svelte-gestures';
+    //import Grid from 'svelte-grid-extended';
+    import Navbar from '$lib/components/Navbar.svelte';
     import MusicPlayer from './bgmusic.svelte';
 	import Slideshow from './Slideshow.svelte';
 	import Topbar from './topbar.svelte';
+	import Menu from './Menu.svelte';
     export let data: PageData;
-    let curview = null;
+    let curView = null;
     let showMenu = false;
-    let showToolbar = false;
+    let showNavbar = false;
     let timer: string | number | NodeJS.Timeout | undefined;
+
+    /*let items = [
+		{ id: '0', x: 0, y: 0, w: 1, h: 1 },
+		{ id: '1', x: 0, y: 1, w: 3, h: 1 }
+	];
+    let itemSize = {height: 70}*/
 
     const handleClick = () => {
         clearTimeout(timer);
@@ -22,43 +31,40 @@
 
     const handleSwipe = (e: CustomEvent) => {
         if (e.detail.direction === "bottom") {
-            showToolbar = true;
+            showNavbar = true;
         }
         else {
-            showToolbar = false;
+            showNavbar = false;
         }
     }
 </script>
 
-{#if showToolbar}
-<div class="navbar bg-primary shadow-lg shadow-primary-content z-50 absolute">
-	<div class="flex-1">
-	  <a href="/" class="btn btn-ghost normal-case text-xl text-primary-content">Photodash</a>
-	</div>
-	<div class="flex-none">
-	  <a href="/settings" class="btn btn-square btn-ghost text-primary-content">
-		<iconify-icon icon="ion:settings-outline" height="36"></iconify-icon>
-	  </a>
-	</div>
-  </div>
-  {/if}
+{#if showNavbar}
+    <Navbar absolute={true} currentConfig={data.configuration.name} />
+{/if}
+{#if $stateStore !== null}
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<main class="h-screen w-screen overflow-y-hidden bg-transparent" on:click={handleClick} use:swipe={{ timeframe: 300, minSwipeDistance: 50, touchAction: 'none' }} on:swipe={handleSwipe}>
+<main class="h-screen w-screen overflow-y-hidden bg-transparent grid grid-rows-[80px_1fr_100px] relative z-10" on:click={handleClick} use:swipe={{ timeframe: 300, minSwipeDistance: 50, touchAction: 'none' }} on:swipe={handleSwipe}>
     <Topbar />
-    {#if $stateStore !== null}
-        {#if data.configuration?.backgroundMusicEntity && data.configuration?.backgroundMusicFile}
-            <MusicPlayer entity={data.configuration.backgroundMusicEntity} url={data.configuration.backgroundMusicFile} />
-        {/if}
-        {#if 
-            (data.configuration?.useLocalPhotos || data.configuration?.googleAlbumId)
-            &&
-            data.configuration.disableSlideShow.every(d => $stateStore?.[d.entity].state !== d.state)
-        }
-            <Slideshow name={data.configuration.name} googleAlbumId={data.configuration.googleAlbumId} />
-        {/if}
-    {:else}
-        <div class="flex w-full h-full items-center justify-center">
-            <div class="btn loading btn-lg">Connecting...</div>
-        </div>
-    {/if}
+    <div> </div>
+    <Menu {curView} />
+    
+    <!--<Grid class="relative z-30" cols={10} rows={8} bind:items={items} {itemSize} let:item>
+        <div class="bg-primary h-full">{item.id}</div>
+    </Grid>-->
 </main>
+{#if data.configuration?.backgroundMusicEntity && data.configuration?.backgroundMusicFile}
+    <MusicPlayer entity={data.configuration.backgroundMusicEntity} url={data.configuration.backgroundMusicFile} />
+{/if}
+{#if 
+    (data.configuration?.useLocalPhotos || data.configuration?.googleAlbumId)
+    &&
+    data.configuration.disableSlideShow.every(d => $stateStore?.[d.entity].state !== d.state)
+}
+    <Slideshow name={data.configuration.name} googleAlbumId={data.configuration.googleAlbumId} />
+{/if}
+{:else}
+    <main class="flex w-screen h-screen items-center justify-center">
+        <div class="btn loading btn-lg">Connecting...</div>
+    </main>
+{/if}

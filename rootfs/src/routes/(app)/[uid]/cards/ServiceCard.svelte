55c333fbceeac69a05a1089b248ea94e016e5361
apop880 <ajpopoutsis@gmail.com>
2023-01-30 07:30:04 +0000

    Initial ServiceCard POC

---
+++ rootfs/src/routes/(app)/[uid]/cards/ServiceCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { stateStore, action } from "$lib/apistore";
+
+    export let config;
+
+    const handleClick = () => {
+        action(config.serviceType, config.target)
+    }
+</script>
+
+<button class="btn btn-primary h-full w-full" on:click={handleClick}>{config.text}</button>

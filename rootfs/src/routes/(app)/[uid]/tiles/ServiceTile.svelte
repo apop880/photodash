24e971602fe0cf0fe9375f67e894331fb9eb8a97
apop880 <ajpopoutsis@gmail.com>
2023-02-01 06:24:23 +0000

    Rename ServiceCard to ServiceTile

---
+++ rootfs/src/routes/(app)/[uid]/tiles/ServiceTile.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { stateStore, action } from "$lib/apistore";
+	import type { ServiceCardConfig } from "$lib/types";
+
+    export let config: ServiceCardConfig;
+    export let height: number;   
+
+    const handleClick = () => {
+        action(config.serviceType, config.target)
+    }
+</script>
+
+<button class="btn btn-primary h-full w-full grid grid-cols-{config?.icon ? 2 : 1} text-lg" on:click={handleClick}>
+    {#if config?.icon}<div class="row-span-2"><iconify-icon icon={config.icon} height="{65 * height}"></iconify-icon></div>{/if}
+    <div>{config.text}</div>
+    {#if config?.secondaryText}<div>{config.secondaryText}</div>{/if}
+</button>

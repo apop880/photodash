1640f4b49847da3012b7b0b10c1ff40c5d330ece
apop880 <ajpopoutsis@gmail.com>
2023-02-07 03:16:57 +0000

    Fix crash when leaving active session

--- rootfs/src/lib/components/Navbar.svelte
+++ rootfs/src/lib/components/Navbar.svelte
@@ -12,12 +12,12 @@
 
 <div class="navbar bg-primary shadow-lg shadow-primary-content z-50" class:absolute transition:fly={{ y: -200, duration: absolute ? 500 : 0}}>
     <div class:flex-1="{!absolute}">
-    <a href="/" class="btn btn-ghost normal-case text-xl text-primary-content">Photodash</a>
+    <a href="/" data-sveltekit-reload={absolute ? "" : "off"} class="btn btn-ghost normal-case text-xl text-primary-content">Photodash</a>
     </div>
     {#if currentConfig}
     <div class="flex-1 text-primary-content">Current Config: {currentConfig}</div>
     <div class="flex-1">
-        <a href={`/config/${$page.params.uid}`} class="text-primary-content flex items-center gap-2 btn btn-ghost">
+        <a href={`/config/${$page.params.uid}`} data-sveltekit-reload={absolute ? "" : "off"} class="text-primary-content flex items-center gap-2 btn btn-ghost">
                 <iconify-icon icon="fluent:form-24-regular" height="36"></iconify-icon>Edit Config
             </a>
     </div>
@@ -30,7 +30,7 @@
     </div>
     {/if}
     <div class="flex-none">
-    <a href="/settings" class="text-primary-content flex items-center gap-2 btn btn-ghost">
+    <a href="/settings" data-sveltekit-reload={absolute ? "" : "off"} class="text-primary-content flex items-center gap-2 btn btn-ghost">
         <iconify-icon icon="ion:settings-outline" height="36"></iconify-icon>Global Settings
     </a>
     </div>

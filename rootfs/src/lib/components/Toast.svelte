9bdce7f0d3ceb945ad3528adcdeb4122fab5e34e
apop880 <ajpopoutsis@gmail.com>
2023-02-18 05:46:00 +0000

    Make toast fade and optionally auto-close

--- rootfs/src/lib/components/Toast.svelte
+++ rootfs/src/lib/components/Toast.svelte
@@ -1,7 +1,22 @@
-<div class="toast">
-    <div class="alert alert-info">
+<script lang="ts">
+	import { onMount } from "svelte";
+    import { fade } from "svelte/transition";
+    export let autoClose = false;
+    let display = true;
+
+    onMount(() => {
+        if (autoClose) {
+            setTimeout(() => display = false, 3000);
+        }
+    })
+</script>
+
+{#if display}
+<div class="toast" transition:fade>
+    <div class="alert alert-success">
         <div>
         <slot></slot>
         </div>
     </div>
 </div>
+{/if}

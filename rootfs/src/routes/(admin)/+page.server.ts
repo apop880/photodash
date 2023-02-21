5c6364c1a50ac11aa354443a6f18015e78bb6906
apop880 <ajpopoutsis@gmail.com>
2023-02-21 22:48:52 +0000

    Sort configuration ascending by name

--- rootfs/src/routes/(admin)/+page.server.ts
+++ rootfs/src/routes/(admin)/+page.server.ts
@@ -2,7 +2,7 @@ import prisma from "$lib/prisma";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async () => {
-    let configurations = await prisma.configuration.findMany({select: { uid: true, name: true }})
+    let configurations = await prisma.configuration.findMany({select: { uid: true, name: true }, orderBy: [ { name: 'asc' } ]})
     
     return { configurations }
 }) satisfies PageServerLoad

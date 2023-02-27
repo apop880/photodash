feac34c7e5039148698bedb525828842bc0e7be2
apop880 <ajpopoutsis@gmail.com>
2023-02-27 22:17:50 +0000

    Remove supervisor URL

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -4,7 +4,6 @@ password=$(bashio::services "mysql" "password")
 port=$(bashio::services "mysql" "port")
 username=$(bashio::services "mysql" "username")
 export DATABASE_URL="mysql://${username}:${password}@${host}:${port}/photodash?schema=public"
-export SUPERVISOR_URL="http://supervisor/core"
 npx prisma db push
 cd build/client
 ln -s /share share
